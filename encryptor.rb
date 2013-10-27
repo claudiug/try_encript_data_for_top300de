b3838da37d32cfd8878d0857831ba45d58c1b8bd
claudiu <claudiu.garba@gmail.com>
2013-10-27 22:13:56 +0100

    bleah

--- encryptor.rb
+++ encryptor.rb
@@ -1,2 +1,21 @@
 class Encryptor
+
+
+  #hash table  :)
+  def cipher(rotation)
+    characters = (' '..'z').to_a
+    rotated_characters = characters.rotate(rotation)
+    Hash[characters.zip(rotated_characters)]
+  end
+
+
+  def encrypt_letter(letter,rotation)
+    cipher_for_rotation = cipher(rotation)
+    cipher_for_rotation[letter]
+  end
+
+
 end
+
+
+
